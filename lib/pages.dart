963493beffa423eae8c7ff1b2934ff9c6edd1747
Matt Carroll <me@mattcarroll.name>
2018-02-19 00:17:59 -0800

    Added hard-coded page viewmodels.

---
+++ lib/pages.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+
+final pages = [
+  new Page(
+    const Color(0xFF678FB4),
+    'assets/hotels.png',
+    'Hotels',
+    'All hotels and hostels are sorted by hospitality rating',
+    'assets/key.png'
+  ),
+  new Page(
+    const Color(0xFF65B0B4),
+    'assets/banks.png',
+    'Banks',
+    'We carefully verify all banks before adding them into the app',
+    'assets/wallet.png'
+  ),
+  new Page(
+    const Color(0xFF9B90BC),
+    'assets/stores.png',
+    'Store',
+    'All local stores are categorized for your convenience',
+    'assets/shopping_cart.png',
+  ),
+];
+
+class Page {
+  final Color color;
+  final String heroAssetPath;
+  final String title;
+  final String body;
+  final String iconAssetPath;
+
+  Page(
+    this.color,
+    this.heroAssetPath,
+    this.title,
+    this.body,
+    this.iconAssetPath,
+  );
+}
