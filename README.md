d30b245c3ffcc7a0f09f5663a53d2fd2427422a1
Matt Carroll <me@mattcarroll.name>
2018-02-19 00:10:35 -0800

    Initial commit. New project with README.

---
+++ README.md
@@ -0,0 +1,7 @@
+# UI Challenge: Material Page Reveal
+
+UI Challenge that reveals pages with a material effect.
+
+This challenge emulates the UI created by Ramotion available here:
+https://github.com/Ramotion/paper-onboarding-android
+
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Material Page Reveal',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+
+      ),
+    );
+  }
+}
